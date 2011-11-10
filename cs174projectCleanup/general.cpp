ff80e8aa697a25c220adacd239e613db16d0ee41
Michael Sechooler <mikesech@gmail.com>
2011-11-09 19:09:03 -0800

    Added key press/release and mouse movement (active and passive) handlers which set the corresponding global state variables to their appropriate values.

--- cs174projectCleanup/general.cpp
+++ cs174projectCleanup/general.cpp
@@ -24,15 +24,43 @@ namespace Globals
 	}
 
 	void callbackReshape(int width, int height){}
-	void callbackKeyboard(unsigned char key, int x, int y){
+	static void setKey(unsigned char key, bool val) {
 		switch (key){
-		case 27: //esc
-			exit(0);
+		case 'A':
+		case 'a':
+			Globals::KEY_W = val;
+			break;
+		case 'W':
+		case 'w':
+			Globals::KEY_A = val;
+			break;
+		case 'S':
+		case 's':
+			Globals::KEY_S = val;
+			break;
+		case 'D':
+		case 'd':
+			Globals::KEY_D = val;
+			break;
+		case 'Q':
+		case 'q':
+			Globals::KEY_Q = val;
+			break;
+		case 'E':
+		case 'e':
+			Globals::KEY_E = val;
+			break;
+		case ' ':
+			Globals::KEY_SPACE = val;
 			break;
 		}
 	}
+	void callbackKeyboard(unsigned char key, int x, int y){
+		if(key == 27) exit(0); //quit on esc
+		else setKey(key, true);
+	}
 	void callbackKeyboardUp(unsigned char key, int x, int y){
-
+		setKey(key, false);
 	}
 	void callbackSpecialFunc(int key, int x, int y){
 
@@ -41,13 +69,15 @@ namespace Globals
 
 	}
 	void callbackMouse(int button, int state, int x, int y){
-
+		
 	}
 	void callbackMotion(int x, int y){
-
+		Globals::mouseX = x;
+		Globals::mouseY = y;
 	}
 	void callbackPassiveMotion(int x, int y){
-
+		Globals::mouseX = x;
+		Globals::mouseY = y;
 	}
 	void callbackIdle(){}
 	void callbackTimer(int) // Called when the timer expires
