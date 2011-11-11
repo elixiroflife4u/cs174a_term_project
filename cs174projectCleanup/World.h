9f2a88ae27d6d2e88d8316e3a1f99a9eae4038e1
Michael Sechooler <mikesech@gmail.com>
2011-11-11 15:31:12 -0800

    Fixed conflicts arising from Garrett's new World.h being based on the old version of the header Removed MobileEntity.cpp from the solution. This revision should both build and run successfully.

--- cs174projectCleanup/World.h
+++ cs174projectCleanup/World.h
@@ -2,126 +2,76 @@
 #define _WORLD
 
 #include "Angel.h"
+namespace Globals
+{
+	//ENGINE BASICS//
+	extern GLuint sProgram;
 
-//ENGINE BASICS//
-GLuint sProgram=-1;
+	//Key States
+	extern bool KEY_W;
+	extern bool KEY_A;
+	extern bool KEY_S;
+	extern bool KEY_D;
+	extern bool KEY_Q;
+	extern bool KEY_E;
+	extern bool KEY_SPACE;
 
-//Key States
-bool KEY_W=false;
-bool KEY_A=false;
-bool KEY_S=false;
-bool KEY_D=false;
-bool KEY_Q=false;
-bool KEY_E=false;
-bool KEY_SPACE=false;
+	extern int mouseX;
+	extern int mouseY;
 
-int mouseX=0;
-int mouseY=0;
+	extern GLuint loc_modelTransMat;
+	extern GLuint loc_camTransMat;
 
+	//material property locations
+	extern GLuint loc_texOffset;
+	extern GLuint loc_texScale;
+	extern GLuint loc_shininess;
+	extern GLuint loc_texture;
 
-///////////NEW//////////
-//transformation locations
-GLuint loc_modelTransMat=0;
-GLuint loc_camTransMat=0;
+	//light property locations
+	extern GLuint loc_ambLightColor;
+	extern GLuint loc_lightPos;
+	extern GLuint loc_lightColor;
+	extern GLuint loc_lightFalloff;
 
-//material property locations
-GLuint loc_texOffset=0;
-GLuint loc_texScale=0;
-GLuint loc_shininess=0;
-GLuint loc_texture=0;
+	void initShaderVariables(GLuint p);
 
-//light property locations
-GLuint loc_ambLightColor=0;
-GLuint loc_lightPos=0;
-GLuint loc_lightColor=0;
-GLuint loc_lightFalloff=0;
+	void setModelTransMatrix(mat4 m);
+	void setCameraTransMatrix(mat4 m);
 
-void initShaderVariables(GLuint p){
-	loc_modelTransMat=glGetUniformLocation(p, "vTransform");
-	loc_camTransMat=glGetUniformLocation(p, "camTransform");
+	void setTextureOffset(vec2 v);
+	void setTextureScale(vec2 v);
+	void setShininess(float f);
+	void setUseTexture();
 
-	loc_texOffset=glGetUniformLocation(p, "uvOffset");
-	loc_texScale=glGetUniformLocation(p, "uvScale");
-	loc_shininess=glGetUniformLocation(p, "shininess");
-	loc_texture=glGetUniformLocation(p, "diffuseMap");
+	void setAmbientLightColor(vec3 v);
+	void setLightPositions(vec3* lightPos,GLuint num);
+	void setLightColors(Angel::vec3* lightCol,GLuint num);
+	void setLightFalloff(float* lightFO, GLuint num);
 
-	loc_ambLightColor=glGetUniformLocation(p, "ambientColor");
-	loc_lightPos=glGetUniformLocation(p, "lightPos");
-	loc_lightColor=glGetUniformLocation(p, "lightColor");
-	loc_lightFalloff=glGetUniformLocation(p, "lightFalloff");
+	//set texture offset
+	//set texture scale
+	//set position of lights/amount of lights/color of lights/falloff of lights
+	//set shininess
+	//set transformation matrix
+	//set camera position
+	
+	//FOR OUR GAME//
+	//Include Bullets
+	//Include Walls
+	//Include MainChar
+	//Include GameEntity
+
+	//Array of Walls
+	//Array of Bullets
+	//Array of GameEntities
+	//Array of Scenes
+	//transparent object heap
+
+	extern vec3 grav;
+
+	extern int currentLevel;
+	//pointer to the current camera
 }
-
-void setModelTransMatrix(mat4 m){
-	glUniformMatrix4fv(loc_modelTransMat,1,GL_FALSE,m);
-}
-void setCameraTransMatrix(mat4 m){
-	glUniformMatrix4fv(loc_camTransMat,1,GL_FALSE,m);
-}
-
-void setTextureOffset(vec2 v){
-	glUniform2f(loc_texOffset,(GLfloat)v.x,(GLfloat)v.y);
-}
-void setTextureScale(vec2 v){
-	glUniform2f(loc_texScale,(GLfloat)v.x,(GLfloat)v.y);
-}
-void setShininess(float f){
-	glUniform1f(loc_shininess,(GLfloat)f);
-}
-void setUseTexture(){
-}
-
-void setAmbientLightColor(vec3 v){
-	glUniform4f(loc_texScale,(GLfloat)v.x,(GLfloat)v.y,(GLfloat)v.z,0.0);
-}
-void setLightPositions(vec3* lightPos,GLuint num){
-	float* val=new GLfloat[num*4];
-	for(int i=0;i<num;i++){
-		val[4*i]=lightPos[i].x;
-		val[4*i+1]=lightPos[i].y;
-		val[4*i+2]=lightPos[i].z;
-		val[4*i+3]=0.0;
-	}
-	glUniform4fv(loc_lightPos,num,val);
-	delete val;
-}
-void setLightColors(Angel::vec3* lightCol,GLuint num){
-	float* val=new GLfloat[num*4];
-	for(int i=0;i<num;i++){
-		val[4*i]=lightCol[i].x;
-		val[4*i+1]=lightCol[i].y;
-		val[4*i+2]=lightCol[i].z;
-		val[4*i+3]=0.0;
-	}
-	glUniform4fv(loc_lightColor,num,val);
-	delete val;
-}
-void setLightFalloff(float* lightFO, GLuint num){
-	glUniform1fv(loc_lightFalloff,num,lightFO);
-}
-
-/////EndNew////////////////////////////////
-
-
-//FOR OUR GAME//
-
-//Include Bullets
-//Include Walls
-//Include MainChar
-//Include GameEntity
-
-//Array of Walls
-//Array of Bullets
-//Array of GameEntities
-//Array of Scenes
-//transparent object heap
-
-vec3 grav;
-
-int currentLevel=0;
-//pointer to the current camera
-
-
-
-
 
 #endif //_WORLD
--- cs174projectCleanup/world.cpp
+++ cs174projectCleanup/world.cpp
@@ -16,6 +16,85 @@ namespace Globals
 	int mouseX=0;
 	int mouseY=0;
 
+	//transformation locations
+	GLuint loc_modelTransMat=0;
+	GLuint loc_camTransMat=0;
+
+	//material property locations
+	GLuint loc_texOffset=0;
+	GLuint loc_texScale=0;
+	GLuint loc_shininess=0;
+	GLuint loc_texture=0;
+
+	//light property locations
+	GLuint loc_ambLightColor=0;
+	GLuint loc_lightPos=0;
+	GLuint loc_lightColor=0;
+	GLuint loc_lightFalloff=0;
+
+	void initShaderVariables(GLuint p){
+		loc_modelTransMat=glGetUniformLocation(p, "vTransform");
+		loc_camTransMat=glGetUniformLocation(p, "camTransform");
+
+		loc_texOffset=glGetUniformLocation(p, "uvOffset");
+		loc_texScale=glGetUniformLocation(p, "uvScale");
+		loc_shininess=glGetUniformLocation(p, "shininess");
+		loc_texture=glGetUniformLocation(p, "diffuseMap");
+
+		loc_ambLightColor=glGetUniformLocation(p, "ambientColor");
+		loc_lightPos=glGetUniformLocation(p, "lightPos");
+		loc_lightColor=glGetUniformLocation(p, "lightColor");
+		loc_lightFalloff=glGetUniformLocation(p, "lightFalloff");
+	}
+
+	void setModelTransMatrix(mat4 m){
+		glUniformMatrix4fv(loc_modelTransMat,1,GL_FALSE,m);
+	}
+	void setCameraTransMatrix(mat4 m){
+		glUniformMatrix4fv(loc_camTransMat,1,GL_FALSE,m);
+	}
+
+	void setTextureOffset(vec2 v){
+		glUniform2f(loc_texOffset,(GLfloat)v.x,(GLfloat)v.y);
+	}
+	void setTextureScale(vec2 v){
+		glUniform2f(loc_texScale,(GLfloat)v.x,(GLfloat)v.y);
+	}
+	void setShininess(float f){
+		glUniform1f(loc_shininess,(GLfloat)f);
+	}
+	void setUseTexture(){
+	}
+
+	void setAmbientLightColor(vec3 v){
+		glUniform4f(loc_texScale,(GLfloat)v.x,(GLfloat)v.y,(GLfloat)v.z,0.0);
+	}
+	void setLightPositions(vec3* lightPos,GLuint num){
+		float* val=new GLfloat[num*4];
+		for(int i=0;i<num;i++){
+			val[4*i]=lightPos[i].x;
+			val[4*i+1]=lightPos[i].y;
+			val[4*i+2]=lightPos[i].z;
+			val[4*i+3]=0.0;
+		}
+		glUniform4fv(loc_lightPos,num,val);
+		delete val;
+	}
+	void setLightColors(Angel::vec3* lightCol,GLuint num){
+		float* val=new GLfloat[num*4];
+		for(int i=0;i<num;i++){
+			val[4*i]=lightCol[i].x;
+			val[4*i+1]=lightCol[i].y;
+			val[4*i+2]=lightCol[i].z;
+			val[4*i+3]=0.0;
+		}
+		glUniform4fv(loc_lightColor,num,val);
+		delete val;
+	}
+	void setLightFalloff(float* lightFO, GLuint num){
+		glUniform1fv(loc_lightFalloff,num,lightFO);
+	}
+
 	//set texture offset
 	//set texture scale
 	//set position of lights/amount of lights/color of lights/falloff of lights
