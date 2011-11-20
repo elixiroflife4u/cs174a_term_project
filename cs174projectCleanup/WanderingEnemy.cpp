b1641913f77720975a107c69d98b84f80e205751
Michael Sechooler <mikesech@gmail.com>
2011-11-20 01:00:43 -0800

    WanderingEnemy rotates in small, discrete steps each frame. It no longer rotates instantly to the desired heading, so rotation is now more smooth.
    Also fixed bug with firing rate limiting so that it behaves properly.
    Added documentation to entirety of class.

--- cs174projectCleanup/WanderingEnemy.cpp
+++ cs174projectCleanup/WanderingEnemy.cpp
@@ -15,6 +15,32 @@ WanderingEnemy::WanderingEnemy(vec3 pos) : MobileEntity(ID_WANDERING_EMEMY), _wa
 	scale(3,5,3);
 }
 
+template <typename T>
+static inline T clamp(T val, T min, T max) {
+	return std::max(std::min(val, max), min);
+}
+
+/** Returns the next angle that an object should face at
+  * if its turning rate is limited.
+  * @param The current angle in degrees.
+  * @param The desired angle in degrees.
+  * @param The maximal difference between the current and next angle.
+  * @return The next angle.
+  */
+static double nextDegree(double current, double desired, double max) {
+	double delta = desired - current;
+	if(delta > 180)
+		delta -= 360;
+	else if(delta < -180)
+		delta += 360;
+
+	const double limited = clamp(delta, -max, max);
+	double newAngle = current + limited;
+	if(newAngle < 0) newAngle += 360;
+	else if(newAngle > 360) newAngle -= 360;
+	return newAngle;
+}
+
 void WanderingEnemy::wander() {
 	//We aren't locked on right now, so reset bullet timer.
 	_bulletDelay = BULLET_DELAY;
@@ -23,12 +49,10 @@ void WanderingEnemy::wander() {
 	if(!_wanderCount) {
 		//rand() doesn't seem to work well for floating point numbers,
 		//so we'll choose an integral degree and convert it to radians.
-		const int xzDegrees = (std::rand() % 360);
-		const double xz = xzDegrees * DegreesToRadians;
+		_desiredHeading = (std::rand() % 360);
+		const double xz = _desiredHeading * DegreesToRadians;
 		const vec3 direction(cos(xz), 0, -sin(xz));
 		setAcc(direction);
-		///@todo Tween the rotation.
-		setRotate(0, xzDegrees, 0);
 
 		_wanderCount = WANDER_COUNT;
 	} else
@@ -37,19 +61,23 @@ void WanderingEnemy::wander() {
 	getModel().setDiffuseColor(0,0,1);
 }
 
-void WanderingEnemy::lockOnPlayer(double xz, double distance) {
-	//Move and rotate enemy towards player
-	///@todo Tween the rotation and slow down.
-	setRotate(0, xz / DegreesToRadians, 0);
+void WanderingEnemy::lockOnPlayer(double xz, double distance, double currentXZ) {
+	_desiredHeading = xz / DegreesToRadians;
+
+	//Move in the direction of the player.
+	//(Not the current rotation because that would cause
+	// the relative heading to change, which makes the movement
+	// erratic.)
+	//Or freeze if too close.
 	if(distance > MIN_DISTANCE)	setAcc(cos(xz), 0, -sin(xz));
 	else setAcc(0, 0, 0);
 	_wanderCount = 0;
 
 	//Fire!
 	const vec4 local(1,0,0,0); //The enemy always fires from its local +X direction.
-	const vec4 direction = RotateY(xz / DegreesToRadians) * local;
+	const vec4 direction = RotateY(currentXZ) * local;
 	const vec3 dirNorm = normalize(vec3(direction.x, direction.y, direction.z));
-	if(_bulletDelay) {
+	if(!_bulletDelay) {
 		Globals::addBullet(ID_BULLET_STRAIGHT,0,1, dirNorm, getModel(1).getTranslate() + 4*dirNorm);
 
 		_bulletDelay = BULLET_DELAY;
@@ -63,6 +91,7 @@ void WanderingEnemy::lockOnPlayer(double xz, double distance) {
 const double WanderingEnemy::FOV = 45 * DegreesToRadians;
 const double WanderingEnemy::MIN_DISTANCE = 15;
 const double WanderingEnemy::MAX_DISTANCE = 50;
+const double WanderingEnemy::MAX_ROTATE = 5;
 
 void WanderingEnemy::update() {
 	static const float MAX_VEL = .5;
@@ -72,6 +101,7 @@ void WanderingEnemy::update() {
 	//the XZ plane. This calculation is independent of
 	//altitude (Y).
 	const Player* const player = Globals::getPlayer();
+	const double currentXZ = getRotate().y;
 	if(player) {
 		const vec3 toPlayer = player->getTranslate() - getTranslate();
 		const double distance = length(toPlayer);
@@ -80,12 +110,12 @@ void WanderingEnemy::update() {
 		//we still want to lock on, just not translate.
 		if(distance < MAX_DISTANCE) {
 			const double xz = std::atan2(-toPlayer.z, toPlayer.x);
-			double xzDelta = (getRotate().y * DegreesToRadians) - xz;
+			double xzDelta = (currentXZ * DegreesToRadians) - xz;
 			if(xzDelta < 0) xzDelta += 2*M_PI;
 			else if(xzDelta > 2*M_PI) xzDelta -= 2*M_PI;
 
 			if(xzDelta < FOV/2 || xzDelta > 2*M_PI - FOV/2)
-				lockOnPlayer(xz, distance);
+				lockOnPlayer(xz, distance, currentXZ);
 			else
 				wander();
 		}
@@ -109,6 +139,9 @@ void WanderingEnemy::update() {
 		setVelZ(horizVel.z);
 	}
 	translate(getVel());
+
+	//Move towards desired heading
+	setRotate(0, nextDegree(currentXZ, _desiredHeading, MAX_ROTATE), 0);
 
 	//Check health
 	if(getHealth()<0){
