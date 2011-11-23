82d3987d35eb6b64bfef8461b2fbe63fdbb8e75b
Michael Sechooler <mikesech@gmail.com>
2011-11-22 22:34:47 -0800

    Initial commit of Framebuffer class. Added CameraEntity::getAspectRatio() to assist in calcuating the height of the Framebuffer viewport.

---
+++ cs174projectCleanup/Framebuffer.cpp
@@ -0,0 +1,47 @@
+#include "Framebuffer.h"
+
+Framebuffer::Framebuffer() {
+	//Initialize objects
+	glGenFramebuffers(1, &_framebuffer);
+	glGenTextures(1, &_texture);
+	glGenRenderbuffers(1, &_zBuffer);
+}
+
+Framebuffer::~Framebuffer() {
+	glDeleteRenderbuffers(1, &_zBuffer);
+	glDeleteTextures(1, &_texture);
+	glDeleteFramebuffers(1, &_framebuffer);
+}
+
+void Framebuffer::render(RenderFunc renderFunc, int width, int height) {
+	//Save the original viewport so we can restore it in the end
+	GLint origViewport[4];
+	glGetIntegerv(GL_VIEWPORT, origViewport);
+
+	glViewport(0,0,width,height);
+
+	//Bind framebuffer and texture
+	glBindFramebuffer(GL_FRAMEBUFFER, _framebuffer);
+	glBindTexture(GL_TEXTURE_2D, _texture);
+
+	//Define and reset texture
+	glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, width, height, 0,GL_RGB, GL_UNSIGNED_BYTE, 0);
+
+	//Create new Z buffer
+	glBindRenderbuffer(GL_RENDERBUFFER, _zBuffer);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, width, height);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _zBuffer);
+
+	//Attach texture to frame buffer object
+	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, _zBuffer, 0);
+
+	//Render stuff as normal
+	renderFunc();
+
+	//Unbind framebuffer and render buffer
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+	//Reset viewport
+	glViewport(origViewport[0], origViewport[1], origViewport[2], origViewport[3]);
+}
