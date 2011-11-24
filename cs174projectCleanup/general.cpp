797ee932caaa8e0392bd3897e3c96441f55a3248
Michael Sechooler <mikesech@gmail.com>
2011-11-24 01:55:24 -0800

    Fixed Text2D problem by disabling GL_GLEND before drawing. Also moved text drawing out of render() and into callbackDisplay() to prevent rendering on the TVScreen.

--- cs174projectCleanup/general.cpp
+++ cs174projectCleanup/general.cpp
@@ -8,7 +8,7 @@
 #include <cassert>
 #include <cstdlib>
 #include <ctime>
-//#include "Text2D.h"
+#include "Text2D.h"
 
 /** This anonymous namespace holds definitions and declarations
   * related to ordering and drawing transparent objects.
@@ -254,33 +254,6 @@ NEXT_J:
 		w.setScale((Globals::getPlayer()->getShieldCharge()/Player::MAX_SHIELD)*.5,.01,.1);
 		w.draw();
 
-		Globals::setModelTransMatrix(mat4());
-		//Text2D n; 
-		//n.draw_stuff("HEALTH",vec4(1,1,1,1), -0.495*(resolution.x/resolution.y), .4725 );
-		//n.draw_stuff("SHIELD",vec4(1,1,1,1), -0.495*(resolution.x/resolution.y), .449 );
-
-		char* weaponText="";
-		switch(getPlayer()->getWeapon()){
-		case 2:
-			weaponText="CURVY BULLET";
-			break;
-		case 0:
-			weaponText="MACHINE GUN";
-			break;
-		case 1:
-			weaponText="MORTAR";
-			break;
-
-		}
-		//n.draw_stuff(weaponText,vec4(1,1,1,1), -0.495*(resolution.x/resolution.y), .449-(.4725-.449) );
-
-		//if(wScenes[currentLevel]->_beaten){
-			//n.draw_stuff("You Won!!",vec4(1,1,1,1),-.025,.05);
-			//n.draw_stuff("Press 'esc' to quit",vec4(1,1,1,1),-.1,0);
-		//}
-
-
-
 		glDisable(GL_BLEND);
 	}
 
@@ -295,6 +268,33 @@ NEXT_J:
 
 		//Render to the screen.
 		render();
+
+		//Draw UI Text
+		Globals::setModelTransMatrix(mat4());
+		Text2D n; 
+		n.draw_stuff("HEALTH",vec4(1,1,1,1), -0.495*(resolution.x/resolution.y), .4725 );
+		n.draw_stuff("SHIELD",vec4(1,1,1,1), -0.495*(resolution.x/resolution.y), .449 );
+
+		char* weaponText="";
+		switch(getPlayer()->getWeapon()){
+		case 2:
+			weaponText="CURVY BULLET";
+			break;
+		case 0:
+			weaponText="MACHINE GUN";
+			break;
+		case 1:
+			weaponText="MORTAR";
+			break;
+
+		}
+		n.draw_stuff(weaponText,vec4(1,1,1,1), -0.495*(resolution.x/resolution.y), .449-(.4725-.449) );
+
+		if(wScenes[currentLevel]->_beaten){
+			n.draw_stuff("You Won!!",vec4(1,1,1,1),-.025,.05);
+			n.draw_stuff("Press 'esc' to quit",vec4(1,1,1,1),-.1,0);
+		}
+
 		glutSwapBuffers();
 	}
 
